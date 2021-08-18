3d802643a2854541f8df7292fa111f97c355742a
ek3ne <83097124+ek3ne@users.noreply.github.com>
2021-08-18 20:20:27 +0100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,41 @@
+# resume2
+#in python
+
+Name = 'EKENE CLARENCE\n'
+Title = 'Backend Developer (Python)\n'
+Contact = ' Victory Estate, Lagos, Nigeria \n +2348166361188 \n ekene.n.clarence@gmail.com\n github.com/3k3n3\n\n'
+print (Name+Title+Contact)
+
+EduHeader = 'EDUCATION\n'
+EduTwoTitle = 'National Open University of Nigeria, Master of Science\n '
+EduTwoTime = '2020-2022\n '
+EduTwoDesc = 'Information Technology\n'
+EduOneTitle = 'Delta State University, Bachelor of Science\n '
+EduOneTime = '2011-2015\n '
+EduOneDesc = 'Biochemistry\n\n'
+
+print(EduHeader+EduTwoTitle+EduTwoTime+EduTwoDesc+EduOneTitle+EduOneTime+EduOneDesc)
+
+ProjectsHeader = 'PROGRAMMING EXPERIENCE (Python)\n'
+ProjectOneTitle = 'Zuri X i4g Internship\n '
+ProjectOneDesc = '2021\n'
+ProjectTwoTitle = 'HNGi8 x i4g Internship\n '
+ProjectTwoDesc = '2021\n\n'
+print(ProjectsHeader+ProjectOneTitle+ProjectOneDesc+ProjectTwoTitle+ProjectTwoDesc)
+
+WorkHeader = 'WORK EXPERIENCE\n'
+WorkOneTitle = 'IE Plc. / Supply Chain Management\n'
+WorkOneTime = 'August 2019-Present\n '
+WorkOneDesc = '- Controlling stocks and Inventory Levels\n - Prepare MIS Reports\n - Comply with QHSE objectives\n\n'
+WorkTwoTitle = 'Technoserve Africa (Nig.) Ltd. / Logistic Operations\n'
+WorkTwoTime = 'May 2017 - July 2018\n '
+WorkTwoDesc = '- Manage correspondence with Clients\n - Track Shipments and Pick-ups\n - Maintain accurate records and documentation\n\n'
+print (WorkHeader+WorkOneTitle+WorkOneTime+WorkOneDesc+WorkTwoTitle+WorkTwoTime+WorkTwoDesc) 
+
+CertCourseHeader = 'CERTIFICATES & COURSES\n'
+CertCourse1 = '-QHSE Competency Development Programme\n'
+CertCourse2 = '-HSE 1 & 2\n'
+CertCourse3 = '-Customer Relationship Management\n'
+CertCourse4 = '-Project Management\n'
+CertCourse5 = '-Soft-Skills Training\n\n'
+print (CertCourseHeader+CertCourse1+CertCourse2+CertCourse3+CertCourse4+CertCourse5)
